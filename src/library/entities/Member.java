d75ea8763d25af95975dd79282dd1df36e1b9727
KevinDinh <dinhvietkhue@gmail.com>
2016-09-15 11:59:11 +1000

    Add files via upload

--- src/library/entities/Member.java
+++ src/library/entities/Member.java
@@ -1,161 +1,185 @@
-/*
- * Decompiled with CFR 0_115.
- */
 package library.entities;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import library.interfaces.entities.EMemberState;
+
 import library.interfaces.entities.ILoan;
 import library.interfaces.entities.IMember;
+import library.interfaces.entities.EMemberState;
 
-public class Member
-implements IMember {
-    private final String firstName;
-    private final String lastName;
-    private final String contactPhone;
-    private final String emailAddress;
-    private final int id;
-    private EMemberState state;
-    private List<ILoan> loanList;
-    private float totalFines;
+public class Member implements IMember {
 
-    public Member(String firstName, String lastName, String contactPhone, String email, int memberID) {
-        if (!this.sane(firstName, lastName, contactPhone, email, memberID)) {
-            throw new IllegalArgumentException("Member: constructor : bad parameters");
-        }
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.contactPhone = contactPhone;
-        this.emailAddress = email;
-        this.id = memberID;
-        this.loanList = new ArrayList<ILoan>();
-        this.totalFines = 0.0f;
-        this.state = EMemberState.BORROWING_ALLOWED;
-    }
+	private final String firstName;
+	private final String lastName;
+	private final String contactPhone;
+	private final String emailAddress;
+	private final int id;
+	
+	private EMemberState state;
+	private List<ILoan> loanList;
+	private float totalFines;
+	
+	public Member(String firstName, String lastName, String contactPhone,
+			String email, int memberID) {
+		if ( !sane(firstName, lastName, contactPhone, email, memberID)) {
+			throw new IllegalArgumentException("Member: constructor : bad parameters");
+		}
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.contactPhone = contactPhone;
+		this.emailAddress = email;
+		this.id = memberID;
+		this.loanList = new ArrayList<ILoan>();
+		this.totalFines = 0.0f;
+		this.state = EMemberState.BORROWING_ALLOWED;
+	}
 
-    private boolean sane(String firstName, String lastName, String contactPhone, String emailAddress, int memberID) {
-        if (!(firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty() || contactPhone == null || contactPhone.isEmpty() || emailAddress == null || emailAddress.isEmpty() || memberID <= 0)) {
-            return true;
-        }
-        return false;
-    }
+	
+	private boolean sane(String firstName, String lastName, String contactPhone,
+			String emailAddress, int memberID) {
+		return  ( firstName != null    && !firstName.isEmpty()    &&
+				  lastName != null     && !lastName.isEmpty()     &&
+				  contactPhone != null && !contactPhone.isEmpty() &&
+				  emailAddress != null && !emailAddress.isEmpty() &&
+				  memberID > 0 
+				);
+	}
 
-    @Override
-    public boolean hasOverDueLoans() {
-        for (ILoan loan : this.loanList) {
-            if (!loan.isOverDue()) continue;
-            return true;
-        }
-        return false;
-    }
 
-    @Override
-    public boolean hasReachedLoanLimit() {
-        boolean b = this.loanList.size() >= 5;
-        return b;
-    }
+	@Override
+	public boolean hasOverDueLoans() {
+		for (ILoan loan : loanList) {
+			if (loan.isOverDue()) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    @Override
-    public boolean hasFinesPayable() {
-        boolean b = this.totalFines > 0.0f;
-        return b;
-    }
+	@Override
+	public boolean hasReachedLoanLimit() {
+		boolean b = loanList.size() >= IMember.LOAN_LIMIT;
+		return b;
+	}
 
-    @Override
-    public boolean hasReachedFineLimit() {
-        boolean b = this.totalFines >= 10.0f;
-        return b;
-    }
+	@Override
+	public boolean hasFinesPayable() {
+		boolean b = totalFines > 0.0f;
+		return b;
+	}
 
-    @Override
-    public float getFineAmount() {
-        return this.totalFines;
-    }
+	@Override
+	public boolean hasReachedFineLimit() {
+		boolean b = totalFines >= IMember.FINE_LIMIT;
+		return b;
+	}
 
-    @Override
-    public void addFine(float fine) {
-        if (fine < 0.0f) {
-            throw new RuntimeException(String.format("Member: addFine : fine cannot be negative", new Object[0]));
-        }
-        this.totalFines += fine;
-        this.updateState();
-    }
+	@Override
+	public float getFineAmount() {
+		return totalFines;
+	}
 
-    @Override
-    public void payFine(float payment) {
-        if (payment < 0.0f || payment > this.totalFines) {
-            throw new RuntimeException(String.format("Member: addFine : payment cannot be negative or greater than totalFines", new Object[0]));
-        }
-        this.totalFines -= payment;
-        this.updateState();
-    }
+	@Override
+	public void addFine(float fine) {
+		if (fine < 0) {
+			throw new RuntimeException(String.format("Member: addFine : fine cannot be negative"));
+		}
+		totalFines += fine;
+		updateState();
+	}
 
-    @Override
-    public void addLoan(ILoan loan) {
-        if (!this.borrowingAllowed().booleanValue()) {
-            throw new RuntimeException(String.format("Member: addLoan : illegal operation in state: %s", new Object[]{this.state}));
-        }
-        this.loanList.add(loan);
-        this.updateState();
-    }
+	@Override
+	public void payFine(float payment) {
+		if (payment < 0 || payment > totalFines) {
+			throw new RuntimeException(String.format("Member: addFine : payment cannot be negative or greater than totalFines"));
+		}
+		totalFines -= payment;
+		updateState();
+	}
 
-    @Override
-    public List<ILoan> getLoans() {
-        return Collections.unmodifiableList(this.loanList);
-    }
+	@Override
+	public void addLoan(ILoan loan) {
+		if (!borrowingAllowed()) {
+			throw new RuntimeException(String.format("Member: addLoan : illegal operation in state: %s", state));
+		}
+		loanList.add(loan);
+		updateState();
+	}
 
-    @Override
-    public void removeLoan(ILoan loan) {
-        if (loan == null || !this.loanList.contains(loan)) {
-            throw new RuntimeException(String.format("Member: removeLoan : loan is null or not found in loanList", new Object[0]));
-        }
-        this.loanList.remove(loan);
-        this.updateState();
-    }
+	@Override
+	public List<ILoan> getLoans() {
+		return Collections.unmodifiableList(loanList);
+	}
 
-    @Override
-    public EMemberState getState() {
-        return this.state;
-    }
+	@Override
+	public void removeLoan(ILoan loan) {
+		if (loan == null || !loanList.contains(loan)) {
+			throw new RuntimeException(String.format("Member: removeLoan : loan is null or not found in loanList"));
+		}
+		loanList.remove(loan);
+		updateState();
+	}
 
-    @Override
-    public String getFirstName() {
-        return this.firstName;
-    }
+	
+	@Override
+	public EMemberState getState() {
+		return state;
+	}
 
-    @Override
-    public String getLastName() {
-        return this.lastName;
-    }
+	
+	@Override
+	public String getFirstName() {
+		return firstName;
+	}
 
-    @Override
-    public String getContactPhone() {
-        return this.contactPhone;
-    }
+	
+	@Override
+	public String getLastName() {
+		return lastName;
+	}
 
-    @Override
-    public String getEmailAddress() {
-        return this.emailAddress;
-    }
+	
+	@Override
+	public String getContactPhone() {
+		return contactPhone;
+	}
 
-    @Override
-    public int getID() {
-        return this.id;
-    }
+	
+	@Override
+	public String getEmailAddress() {
+		return emailAddress;
+	}
 
-    public String toString() {
-        return String.format("Id: %d\nName: %s %s\nContact Phone: %s\nEmail: %s\nOutstanding Charges: %0.2f", this.id, this.firstName, this.lastName, this.contactPhone, this.emailAddress, Float.valueOf(this.totalFines));
-    }
+	
+	@Override
+	public int getID() {
+		return id;
+	}
 
-    private Boolean borrowingAllowed() {
-        boolean b = !this.hasOverDueLoans() && !this.hasReachedFineLimit() && !this.hasReachedLoanLimit();
-        return b;
-    }
+	
+	@Override
+	public String toString() {
+		return String.format(
+				"Id: %d\nName: %s %s\nContact Phone: %s\nEmail: %s\nOutstanding Charges: %0.2f", id,
+				firstName, lastName, contactPhone, emailAddress, totalFines);
+	}
 
-    private void updateState() {
-        this.state = this.borrowingAllowed() != false ? EMemberState.BORROWING_ALLOWED : EMemberState.BORROWING_DISALLOWED;
-    }
+	private Boolean borrowingAllowed() {
+		boolean b = !hasOverDueLoans() &&
+				!hasReachedFineLimit() &&
+				!hasReachedLoanLimit();
+		return b;
+	}
+
+	private void updateState() {
+		if (borrowingAllowed()) {
+			state = EMemberState.BORROWING_ALLOWED;
+		}
+		else {
+			state = EMemberState.BORROWING_DISALLOWED;
+		}
+	}
+
+
 }
-
