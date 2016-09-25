606df942731ddaa5030604811149e6eb23d5f708
wingkuitsoi <edmondtsoi92@gmail.com>
2016-09-25 14:38:32 +1000

    Version 1.2 (25-9-2016)
    Tsoi Wing Kui

--- src/library/CardSwipedTestCase2.java
+++ src/library/CardSwipedTestCase2.java
@@ -3,7 +3,6 @@
 
 package library;
 
-import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import library.daos.BookHelper;
@@ -16,17 +15,17 @@ import library.hardware.CardReader;
 import library.hardware.Display;
 import library.hardware.Printer;
 import library.hardware.Scanner;
-import library.interfaces.entities.EMemberState;
+import library.interfaces.entities.IBook;
 import library.interfaces.entities.IMember;
 
-public class CardSwipedTestCase1 {
+public class CardSwipedTestCase2 {
 
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	// This test is BBUC_Op2: Swipe Borrower Card. 
-	// Testing preConditions.
+	// Testing postConditions: member exists and not restricted.
 	@Test
 	public void testCardSwiped() {
 		
@@ -36,26 +35,27 @@ public class CardSwipedTestCase1 {
 		Printer printer = new Printer(); 
 		Display display = new Display(); 
 		BookHelper a = new BookHelper();
-		BookMapDAO bookDAO = new BookMapDAO(a); 
+		BookMapDAO bookDAO = new BookMapDAO(a);
 		LoanHelper b = new LoanHelper();
 		LoanMapDAO loanDAO = new LoanMapDAO(b);
 		MemberHelper c = new MemberHelper();
 		MemberMapDAO memberDAO = new MemberMapDAO(c);
-		BorrowUC_CTL bookScan = new BorrowUC_CTL(reader, scanner, printer, display, bookDAO, loanDAO, memberDAO); 
+		BorrowUC_CTL bookScan = new BorrowUC_CTL(reader, scanner, printer, display, bookDAO, loanDAO, memberDAO);
 		
-		// Initiate a testing memberID.
-		MemberHelper ma = new MemberHelper();
-		MemberMapDAO mb = new MemberMapDAO(ma);
-		IMember member = mb.addMember("Wing", "Tsoi", "0420654682", "edmondtsoi92@gmail.com");
+		// Create a new book and initiate the bookID.
+		IBook book = bookDAO.addBook("author", "title", "callNo");
+		int bookID = book.getID();
+		
+		// Create a new member and initiate the memberID.
+		IMember member = memberDAO.addMember("Wing", "Tsoi", "0420654682", "edmondtsoi92@gmail.com");
 		int memberID = member.getID();
-		// Test passed:
-		// assertEquals(EMemberState.BORROWING_ALLOWED, member.getState());
 		
-		// Test cardSwiped method.
+		// Test cardSwiped method and bookScanned method.
 		bookScan.initialise();
-		bookScan.cardSwiped(memberID);
+		bookScan.cardSwiped(memberID);		
+		bookScan.bookScanned(bookID);
 	}
 
 }
 
-// The result returns "INITIALIZED" state as expected.
+// The result returns "SCANNING_BOOKS" state as expected.
